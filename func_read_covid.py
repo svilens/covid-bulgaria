0316e8d62953a7048bd94b333fd7dc8fc0603428
AWS EC2 <ec2@user.aws>
2022-01-09 23:23:34 +0000

    vaccines key

--- func_read_covid.py
+++ func_read_covid.py
@@ -85,6 +85,7 @@ def read_breakthrough(path):
         .replace('MOD - COM', 'Pfizer')
         .replace('MOD - JANSS', 'Johnson & Johnson')
         .replace('JANSS - MOD', 'Moderna')
+        .replace('AZ - COM - MOD', 'Moderna')
     )
     return df
 
